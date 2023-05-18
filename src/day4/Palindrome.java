fc2618e66a044d6675cd7e8e7780ab00838f4cad
upendra <upendrasahu1199@gmail.com>
2023-05-18 13:50:14 +0530

    Check given input is palindrome or not

---
+++ src/day4/Palindrome.java
@@ -0,0 +1,31 @@
+package day4;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class Palindrome {
+    String input;
+
+    public Palindrome(String input) {
+        this.input = input;
+    }
+
+    public void checkPalindrome() {
+        int count = 0;
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.toLowerCase().charAt(i) == input.toLowerCase().charAt(input.length() - 1 - i)) {
+                count++;
+            }
+        }
+        if (count == input.length() / 2) {
+            System.out.println("Given input is a Palindrome");
+        } else {
+            System.out.println("Given input is not a Palindrome");
+        }
+    }
+
+    public static void main(String[] args) {
+        Palindrome input1 = new Palindrome("Tata");
+        input1.checkPalindrome();
+    }
+}
